4121c2cf69ed23f2ff2ca134d805d2fe23e05cfa
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-04-01 23:54:35 +0300

    style: design navbar component

---
+++ search-films/src/components/atoms/logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex align-center">
+    <h2>
+      {{ content.title }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * A @component that produces a logo
+   * accepts a content object as props
+   * @example <logo :content="logoContent" />
+   */
+  name: "Logo",
+  /**
+   * The content that helps customize logo
+   * @property: title
+   */
+  props: {
+    content: {
+      type: Object,
+      default() {
+        return {
+          title: "Film DB",
+        };
+      },
+    },
+  },
+  data: () => ({}),
+};
+</script>
