99e723186ed8202b99070762b6cc2521b85c37c5
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-04-02 21:48:38 +0300

    chore: add btn group to the navbar component

---
+++ search-films/src/components/molecules/btn-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn :href="content.path" target="_blank" text>
+    <span class="mr-2">{{ content.title }}</span>
+    <v-icon>{{ content.icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+/**
+ * A @component that produces a button group
+ * accepts a content object as props
+ * @example <btn-group :content="content"></btn-group>
+ */
+export default {
+  name: "BtnGroup",
+  props: {
+    /**
+     * The content that helps customize btn-group
+     * @property: path, title, icon
+     */
+    content: {
+      type: Object,
+      default() {
+        return {
+          path: "/",
+          title: "Check out",
+          icon: "mdi-open-in-new",
+        };
+      },
+    },
+  },
+  data: () => ({}),
+};
+</script>
--- search-films/src/components/organisms/navbar.vue
+++ search-films/src/components/organisms/navbar.vue
@@ -2,11 +2,14 @@
   <v-app-bar app color="grey darken-4" dark>
     <!-- Logo Group-->
     <logo :content="logoContent" />
+    <v-spacer></v-spacer>
+    <btn-group :content="navbarBtnContent"></btn-group>
   </v-app-bar>
 </template>
 
 <script>
 import { Logo } from "../atoms";
+import { BtnGroup } from "../molecules";
 
 export default {
   /**
@@ -16,11 +19,17 @@ export default {
   name: "Navbar",
   components: {
     Logo,
+    BtnGroup,
   },
   data: () => ({
     // content for logo
     logoContent: {
       title: "Film DB",
+    },
+    navbarBtnContent: {
+      path: "https://github.com/mehmeteyupoglu/search-films",
+      title: "Check out the GitHub repo",
+      icon: "mdi-open-in-new",
     },
   }),
 };
